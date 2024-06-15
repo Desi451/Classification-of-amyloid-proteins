6da0cfaa30a1a5c400f1dbaec0122806ed931ce3
sztynksior <szszedel@gmail.com>
2024-06-15 16:18:50 +0200

    zmiana paru nazw plikow skryptow

---
+++ data_prep/aaindex2or3_data_extraction.R
@@ -0,0 +1,71 @@
+# Ścieżka do pliku tekstowego
+file_path <- "data/raw_data/aaindex2.txt"
+
+# Odczytanie pliku linia po linii
+lines <- readLines(file_path)
+
+# Inicjalizacja zmiennej do przechowywania flagi, czy jesteśmy wewnątrz interesującego nas fragmentu
+inside_section <- FALSE
+
+# Inicjalizacja listy do przechowywania wyodrębnionych fragmentów
+extracted_sections <- list()
+current_section <- c()
+
+# Przetwarzanie każdej linii osobno
+for (line in lines) {
+    if (grepl("^M rows = ARNDCQEGHILKMFPSTWYV, cols = ARNDCQEGHILKMFPSTWYV", line)) {
+        inside_section <- TRUE
+    }
+
+    if (inside_section) {
+        current_section <- c(current_section, line)
+        if (grepl("^//", line)) {
+            inside_section <- FALSE
+            extracted_sections <- append(extracted_sections, list(current_section))
+            current_section <- c()
+        }
+    }
+}
+
+# Przetwarzanie sekcji
+process_section <- function(section) {
+    # Inicjalizacja pustej ramki danych
+    df <- data.frame(stringsAsFactors = FALSE)
+
+    # Usuń nagłówki
+    section <- section[2:length(section)]
+
+    # Ustal długość najdłuższego wiersza
+    max_length <- max(sapply(strsplit(section, "\\s+"), length))
+
+    # Przetwarzanie każdego wiersza
+    for (line in section) {
+        # Usuń dodatkowe białe znaki
+        line <- trimws(line)
+
+        # Podziel wiersz na wartości
+        values <- as.numeric(strsplit(line, "\\s+")[[1]])
+
+        # Uzupełnij brakujące wartości NA
+        values <- c(values, rep(NA, max_length - length(values)))
+
+        # Utwórz ramkę danych dla pojedynczego wiersza
+        row_df <- data.frame(t(values), stringsAsFactors = FALSE)
+
+        # Dołącz ramkę danych do wynikowej ramki
+        df <- rbind(df, row_df)
+    }
+
+    return(df)
+}
+
+# Przetwarzanie wszystkich sekcji
+all_data <- do.call(rbind, lapply(extracted_sections, process_section))
+
+# Ścieżka do pliku CSV
+output_csv_path <- "data/intermediate_data/extracted_values_aaindex2.csv"
+
+# Zapisanie do pliku CSV
+write.csv(all_data, file = output_csv_path, row.names = FALSE, col.names = FALSE)
+
+cat("Wyodrębnione dane zostały zapisane do pliku:", output_csv_path)
--- data_prep/aaindex2or3_data_prep.R
+++ data_prep/aaindex2or3_data_prep.R
@@ -1,71 +1,42 @@
-# Ścieżka do pliku tekstowego
-file_path <- "data/raw_data/aaindex2.txt"
-
-# Odczytanie pliku linia po linii
-lines <- readLines(file_path)
-
-# Inicjalizacja zmiennej do przechowywania flagi, czy jesteśmy wewnątrz interesującego nas fragmentu
-inside_section <- FALSE
-
-# Inicjalizacja listy do przechowywania wyodrębnionych fragmentów
-extracted_sections <- list()
-current_section <- c()
-
-# Przetwarzanie każdej linii osobno
-for (line in lines) {
-    if (grepl("^M rows = ARNDCQEGHILKMFPSTWYV, cols = ARNDCQEGHILKMFPSTWYV", line)) {
-        inside_section <- TRUE
+input_data <- read.csv("data/intermediate_data/extracted_values_aaindex2.csv", header = FALSE)
+input_data$V21 <- NULL
+for (i in 1:nrow(input_data)) {
+    if (input_data[i, 1] == "X1" || is.na(input_data[i, 1])) {
+        input_data <- input_data[-i, ]
     }
+}
+rownames(input_data) <- NULL
 
-    if (inside_section) {
-        current_section <- c(current_section, line)
-        if (grepl("^//", line)) {
-            inside_section <- FALSE
-            extracted_sections <- append(extracted_sections, list(current_section))
-            current_section <- c()
+output_data <- data.frame(matrix(nrow = 0, ncol = 400))
+aminoacid_names <- c("A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V")
+col_names <- c()
+for (i in aminoacid_names) {
+    for (j in aminoacid_names) {
+        col_names <- c(col_names, sprintf("%s%s", j, i))
+    }
+}
+colnames(output_data) <- col_names
+
+output_data_row_count <- 0
+output_data_col_count <- 0
+for (i in 1:nrow(input_data)) {
+    if (i %% 20 == 1) {
+        output_data_row_count <- output_data_row_count + 1
+        output_data_col_count <- 0
+    }
+    for (j in 1:ncol(input_data)) {
+        output_data_col_count <- output_data_col_count + 1
+        output_data[output_data_row_count, output_data_col_count] <- input_data[i, j]
+    }
+}
+
+col_names_out_df <- colnames(output_data)
+for (i in 1:nrow(output_data)) {
+    for (j in 1:ncol(output_data)) {
+        if (is.na(output_data[i, j])) {
+            name_vec <- unlist(strsplit(col_names_out_df[j], ""))
+            col_name_to_search <- sprintf("%s%s", name_vec[2], name_vec[1])
+            output_data[i, j] <- output_data[i, col_name_to_search]
         }
     }
 }
-
-# Przetwarzanie sekcji
-process_section <- function(section) {
-    # Inicjalizacja pustej ramki danych
-    df <- data.frame(stringsAsFactors = FALSE)
-
-    # Usuń nagłówki
-    section <- section[2:length(section)]
-
-    # Ustal długość najdłuższego wiersza
-    max_length <- max(sapply(strsplit(section, "\\s+"), length))
-
-    # Przetwarzanie każdego wiersza
-    for (line in section) {
-        # Usuń dodatkowe białe znaki
-        line <- trimws(line)
-
-        # Podziel wiersz na wartości
-        values <- as.numeric(strsplit(line, "\\s+")[[1]])
-
-        # Uzupełnij brakujące wartości NA
-        values <- c(values, rep(NA, max_length - length(values)))
-
-        # Utwórz ramkę danych dla pojedynczego wiersza
-        row_df <- data.frame(t(values), stringsAsFactors = FALSE)
-
-        # Dołącz ramkę danych do wynikowej ramki
-        df <- rbind(df, row_df)
-    }
-
-    return(df)
-}
-
-# Przetwarzanie wszystkich sekcji
-all_data <- do.call(rbind, lapply(extracted_sections, process_section))
-
-# Ścieżka do pliku CSV
-output_csv_path <- "data/intermediate_data/extracted_values_aaindex2.csv"
-
-# Zapisanie do pliku CSV
-write.csv(all_data, file = output_csv_path, row.names = FALSE, col.names = FALSE)
-
-cat("Wyodrębnione dane zostały zapisane do pliku:", output_csv_path)
