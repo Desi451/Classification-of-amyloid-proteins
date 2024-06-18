4e9fd8f38aea3f3ac5b84c59ea0424efa6ef80f8
Błażej <hunter13cie@gmail.com>
2024-06-18 17:35:06 +0200

    Calculating metrics

--- model/Budapest_My_Model_SVM_training.R
+++ model/Budapest_My_Model_SVM_training.R
@@ -2,6 +2,8 @@ library(caTools)
 library(e1071)
 library(caret)
 library(pROC)
+library(MLmetrics)
+library(irr)
 
 save_model <- FALSE
 
@@ -11,7 +13,7 @@ data_normalized_path <- "data/final_data/final_data_encoded_normalized.csv"
 data_aaindex23_normalized_path <- "data/final_data/aaindex23_data_encoded_normalized.csv"
 data_aaindex1_normalized_path <- "data/final_data/aaindex1_data_encoded_normalized.csv"
 data_one_hot_encoded <- "data/final_data/encoded_hexapeptide_data.csv"
-data_frame <- read.csv2(data_aaindex1_normalized_path, sep = ";", dec = "." , header = TRUE)
+data_frame <- read.csv2(data_normalized_path, sep = ";", dec = "." , header = TRUE)
 
 set.seed(103)
 
@@ -33,17 +35,32 @@ svm_model <- train(Classification ~ .,
 
 prediction <- predict(svm_model, newdata = test_set, type = "prob")
 predicted_classes <- ifelse(prediction[, 2] > 0.5, "amyloid", "non_amyloid")
+predicted_factor <- factor(predicted_classes, levels = c("non_amyloid", "amyloid"))
 
 confusion_matrix <- table(test_set$Classification, predicted_classes)
 print(confusion_matrix)
 
+sens <- sensitivity(predicted_factor, test_set$Classification)
+print(sens)
+
+spec <- specificity(predicted_factor, test_set$Classification)
+print(spec)
+
+prec <- precision(predicted_factor, test_set$Classification)
+print(prec)
+
 accuracy <- sum(predicted_classes == test_set$Classification) / nrow(test_set)
+print(accuracy)
 
 roc_object <- roc(test_set$Classification, as.numeric(prediction[,2]))
 plot(roc_object, col="red", main="ROC curve SVM")
 auc(roc_object)
 
-print(accuracy)
+f1 <- F1_Score(predicted_classes, test_set$Classification)
+print(f1)
+
+golden_kappa <- kappa2(data.frame(test_set$Classification, predicted_classes))
+print(golden_kappa$value)
 
 if (save_model) {
     model_name <- "linear_normalized_aaindex1_c10"
